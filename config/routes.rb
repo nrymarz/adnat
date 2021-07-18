1fe716a2e111aaeec86f2d63948eb280d3823666
Nathan Rymarz <nrymarz@gmail.com>
2021-07-17 20:52:46 -0500

    edited signup form and created login form

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get 'login' to 'sessions#login'
-  post 'logout' to 'sessions#logout'
+  get 'login',to: 'sessions#login'
+  post 'logout', to: 'sessions#logout'
   resources :users, only:[:create,:show,:new]
 end
